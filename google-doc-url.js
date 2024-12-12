5df1df251f52e98e6faa215d1ed360ad045b0c58
terezav20 <160124392+terezav20@users.noreply.github.com>
2024-12-12 12:15:22 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1gNDGURsDLzDKUJouB0TZj3buRAhTX-qnH9YsaLCbIWk/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/12wt7mAKm03306lZdXHHlrAt3koH3Vb7J0kp87NJovE0/edit#gid=0';
  
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
